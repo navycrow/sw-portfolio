62f0b405705c5cbd8b2bb38fbf0ffcd9dea5c0e7
navycrow <hg64off@gmail.com>
2025-05-19 15:08:22 +0200

    correcction bug menu burger

--- assets/js/burger-menu.js
+++ assets/js/burger-menu.js
@@ -1,31 +1,40 @@
 const btn = document.getElementById('burger-menu')
 const menu = document.getElementById('menu')
 
-export function toggleMenu() {
-    const menu = document.getElementById('menu')
 
-}
+// Evénement sur clic d'un élément du document
+document.addEventListener('click', (e) => {
+    console.log('Elément cliqué', e.target)
+    console.log('Menu burger visible :', btn.checkVisibility())
 
+    // Si c'est le menu burger qui est cliqué, affiche/masque le menu suivant l'état actuel
+    if (e.target === btn) {
 
+        const isVisible = menu.checkVisibility()
+        console.log('Menu visible', isVisible, '=>', !isVisible);
 
+        if (isVisible) {
+            menu.style.display = 'none'
+        } else {
+            menu.style.display = 'block'
+        }
+    }
 
-btn.addEventListener('click', (e) => {
-    const isVisible = menu.checkVisibility()
-    console.log('Menu visible', isVisible, '=>', !isVisible);
+    // Si c'est un autre élément de la page, masque le menu
+    else if (btn.checkVisibility()) {
+        menu.style.display = 'none'
+    }
 
-    if (isVisible) {
+})
+
+// Evement sur redimensionnement de la fenêtre
+window.addEventListener('resize', () => {
+    console.log('La fenêtre a été redimensionnée');
+
+    // Si le menu-burger n'est présent, on masque le menu sinon on l'affiche
+    if (btn.checkVisibility()) {
         menu.style.display = 'none'
     } else {
         menu.style.display = 'block'
     }
-})
-
-
-menu.addEventListener('click', (e) => {
-    menu.style.display = 'none'
-})
-
-document.addEventListener('click', (e) => {
-    console.log(menu, e.target)
-    
-})
+});
