017604636ba05bedf31de2fd8001b169bd48260f
navycrow <hg64off@gmail.com>
2025-05-21 14:51:27 +0200

    modif App.jsx, ajout de pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Works from "./pages/Works";
+import Trainings from "./pages/Trainings";
+import Experiences from "./pages/Experiences";
+import Contact from "./pages/Contact";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 
@@ -15,10 +17,13 @@ function App() {
                 <Header />
 
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<About />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/skills" element={<Skills />} />
                     <Route path="/works" element={<Works />} />
+                    <Route path="/trainings" element={<Trainings />} />
+                    <Route path="/experiences" element={<Experiences />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
 
                 <Footer />
