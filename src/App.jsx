5723eb167289f9f2a0608cc1c6ec8fac64fba587
navycrow <hg64off@gmail.com>
2025-05-21 14:43:31 +0200

    complétion Header Footer App et CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Works from "./pages/Works";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 function App() {
+    return (
+        <>
+            <Router>
+                <Header />
 
-  return (
-    <>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/skills" element={<Skills />} />
+                    <Route path="/works" element={<Works />} />
+                </Routes>
 
-    </>
-  )
+                <Footer />
+            </Router>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    const [about, setAbout] = useState({});
+
+    useEffect(() => {
+        fetch("src/data/profile.json")
+            .then((response) => response.json())
+            .then((data) => setAbout(data.aPropos))
+            .catch((err) => console.error("Erreur de chargement JSON :", err));
+    }, []);
+
+    return (
+        <header>
+            <img
+                id="photo"
+                src={about.photo}
+                alt={`${about.prenom} ${about.nom}`}
+                className="rounded-full"
+            />
+
+            <div id="titre">
+                <h3>{`${about.prenom} ${about.nom}`}</h3>
+                <p>{about.poste}</p>
+            </div>
+
+            {/* <!-- Bouton mode sombre et menu burger --> */}
+            <div id="buttons">
+                <button
+                    id="dark-mode-btn"
+                    className="icon"
+                    aria-label="Activer le mode sombre"
+                ></button>
+                <button id="burger-menu" className="icon">
+                    ☰
+                </button>
+            </div>
+
+            {/* <!-- Navbar --> */}
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <Link to="/about">A propos de moi</Link>
+                    </li>
+                    <li>
+                        <Link to="/skills">Mes compétences</Link>
+                    </li>
+                    <li>
+                        <Link to="/works">Mes réalisations</Link>
+                    </li>
+                    <li>
+                        <Link to="trainings">Formations</Link>
+                    </li>
+                    <li>
+                        <Link to="/experiences">
+                            Expériences professionnelles
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
