8081ba4efbf3a60e75bde54255e42104d4fd5fca
navycrow <hg64off@gmail.com>
2025-05-22 11:38:27 +0200

    modification des boutons Burger et Mode Sombre pour améliorer la gestion de l'état et la lisibilité du code

--- src/components/BurgerBtn.jsx
+++ src/components/BurgerBtn.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const BurgerBtn = () => {
+    // Définit l'état du menu, fermé par défaut
+    const [isMenuVisible, setMenu] = useState(false);
+
+    useEffect(() => {
+        const menu = document.getElementById("menu");
+        console.log("Menu visible :", isMenuVisible);
+
+        // Affecte la classe hidden au menu suivant l'état
+        if (isMenuVisible) {
+            menu.classList.remove("hidden");
+        } else {
+            menu.classList.add("hidden");
+        }
+    }, [isMenuVisible]);
+
     return (
-        <button id="burger-menu" className="icon">
-            ☰
+        <button
+            id="burger-menu"
+            className="icon"
+            onClick={() => setMenu(!isMenuVisible)} // change l'état
+        >
+            {isMenuVisible ? "🗙" : "☰"}
         </button>
     );
 };
--- src/components/DarkModeBtn.jsx
+++ src/components/DarkModeBtn.jsx
@@ -1,35 +1,35 @@
 import { useState, useEffect } from "react";
 
 const DarkModeBtn = () => {
-    // Au montage, initialise le darkMode depuis le localStorage,
+    // Au montage, initialise le isDarkMode depuis le localStorage,
     // si la valeur n'existe, applique le mode clair par défaut
-    const [darkMode, setDarkMode] = useState(
-        () => JSON.parse(localStorage.getItem("darkMode")) || false
+    const [isDarkMode, setDarkMode] = useState(
+        () => JSON.parse(localStorage.getItem("isDarkMode")) || false
     );
 
     // Sur chaque changement de l'état
     useEffect(() => {
-        console.log("Dark mode :", darkMode);
+        console.log("Dark mode :", isDarkMode);
 
         // Applique la classe correspondante à la racine du document
-        if (darkMode) {
+        if (isDarkMode) {
             document.documentElement.classList.add("dark");
         } else {
             document.documentElement.classList.remove("dark");
         }
 
         // Stocke le nouvel état dans le localStorage
-        localStorage.setItem("darkMode", JSON.stringify(darkMode));
-    }, [darkMode]);
+        localStorage.setItem("isDarkMode", JSON.stringify(isDarkMode));
+    }, [isDarkMode]);
 
     return (
         <button
             id="dark-mode-btn"
             className="icon"
             aria-label="Mode sombre/clair"
-            onClick={() => setDarkMode(!darkMode)} // Inverse l'état
+            onClick={() => setDarkMode(!isDarkMode)} // Inverse l'état
         >
-            {darkMode ? "☀︎" : "⏾"}
+            {isDarkMode ? "☀︎" : "⏾"}
         </button>
     );
 };
