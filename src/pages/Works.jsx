7318cecf629b4a247158b9a24fca49c18f2827fe
navycrow <hg64off@gmail.com>
2025-05-23 14:38:19 +0200

    ajout réalisations

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./Works.css";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+    const [works, setTrainings] = useState([]);
 
-export default Works
+    useEffect(() => {
+        fetch("src/data/works.json")
+            .then((response) => response.json())
+            .then((data) => setTrainings(data))
+            .catch((err) => console.error("Erreur de chargement JSON :", err));
+    }, []);
+
+    return (
+        <>
+            <section id="works">
+                <h2>Mes réalisations</h2>
+                <div className="container">
+                    {works.map((work, index) => (
+                        <div key={index} className="card">
+                            <h3>{work.titre}</h3>
+                            <a href={work.lien} target="_blank">
+                                <img
+                                    className="screenshot"
+                                    src={work.image}
+                                    alt={work.titre}
+                                />
+                            </a>
+                            <p>{work.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Works;
