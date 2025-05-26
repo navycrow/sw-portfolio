650229c8cc6cf152a152635695f2f0460063b447
navycrow <hg64off@gmail.com>
2025-05-26 18:34:27 +0200

    implantation popup-card (en cours)

--- src/components/PopupCard.jsx
+++ src/components/PopupCard.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-import './PopupCard.css'
+import "./PopupCard.css";
 
 const PopupCard = ({ experience }) => {
-    return (
-        <div className="popup-card hidden">
-            <h3>{experience.poste}</h3>
-            <p>{experience.etablissement}</p>
-            <ul>
-                {experience.fonctions.map((fonction, i) => (
-                    <li key={i}>{fonction}</li>
-                ))}
-            </ul>
-            <p>{experience.periode}</p>
-        </div>
-    );
+    // On vérifie que la clé n'est pas vide
+    if (Object.keys(experience).length > 0) {
+        return (
+            <>
+                <div className="popup-overlay"></div>
+                <div className="popup-card">
+                    <p className="close-btn">
+                        <strong>X</strong>
+                    </p>
+                    <h3>{experience.poste}</h3>
+                    <p>{experience.etablissement}</p>
+                    <ul>
+                        {experience.fonctions.map((fonction, i) => (
+                            <li key={i}>{fonction}</li>
+                        ))}
+                    </ul>
+                    <p>{experience.periode}</p>
+                </div>
+            </>
+        );
+
+        // Sinon retourne RIEN
+    } else {
+        return <></>;
+    }
 };
 
 export default PopupCard;
--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -3,18 +3,15 @@ import PopupCard from "../components/PopupCard";
 
 const Experiences = () => {
     const [experiences, setExperiences] = useState([]);
-    const [moreCard, setMoreCard] = useState(-1);
+    const [experienceDetail, setExperienceDetail] = useState({});
 
+    // Récupération des données
     useEffect(() => {
         fetch("/data/experiences.json")
             .then((response) => response.json())
             .then((data) => setExperiences(data))
             .catch((err) => console.error("Erreur de chargement JSON :", err));
     }, []);
-
-    useEffect(() => {
-        console.log("test");
-    }, [moreCard]);
 
     return (
         <section id="experiences">
@@ -25,29 +22,16 @@ const Experiences = () => {
                         <h3>{experience.poste}</h3>
                         <p>{experience.etablissement}</p>
                         <p>{experience.periode}</p>
-                        <p class="see-more">En savoir plus...</p>
+                        <p
+                            className="see-more"
+                            onClick={() => setExperienceDetail(experience)}
+                        >
+                            En savoir plus...
+                        </p>
                     </div>
                 ))}
             </div>
-            <PopupCard
-                experience={{
-                    poste: "Administrateur systèmes et réseaux",
-                    etablissement: "DARRIEUMERLOU",
-                    fonctions: [
-                        "Installation de nouveaux matériels et équipements informatiques (logiciels, antivirus, imprimantes, ordinateurs …), réalisation des mises à jour.",
-                        "Diagnostic et résolution des incidents sur logiciels, matériels, systèmes, téléphonie et réseaux.",
-                        "Installation des ordinateurs et configuration des postes de travail",
-                        "Gestion du parc informatique (15 postes, 6 imprimantes, 1 serveur, 3 commandes numériques), de la téléphonie fixe et mobile.",
-                        "Renseignement des utilisateurs sur les bonnes pratiques en matière de sécurité informatique.",
-                        "Commande, réception et installation du matériel : PC, tablettes, smartphones, téléphones.",
-                        "Conception et programmation d'applications et de solutions logicielles répondant à des problématiques de l'entreprise",
-                        "Exploitation de base de données, extraction, analyse",
-                        "Outils utilisés : Access/Excel + VBA, Python",
-                        "Connaissances : Powershell, Javascript, AutoIt",
-                    ],
-                    periode: "Sept. 1997 / Août. 2024",
-                }}
-            />
+            <PopupCard experience={experienceDetail} />
         </section>
     );
 };
