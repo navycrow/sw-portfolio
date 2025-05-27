5ce5e42836d7643696aeceb45925e458db2e2b2d
navycrow <hg64off@gmail.com>
2025-05-27 08:07:50 +0200

    ajout commentaire popup

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const Experiences = () => {
     const [experiences, setExperiences] = useState([]);
-    const [selectedItem, selectItem] = useState(-1);
+    const [selectedItem, selectItem] = useState(-1); /* pour l'affichage de la popup */
 
     // Récupération des données
     useEffect(() => {
