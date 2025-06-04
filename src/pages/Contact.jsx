9c85c8da5dca4581c808377357ca73af53c4c6c3
navycrow <hg64off@gmail.com>
2025-06-04 16:24:26 +0200

    mise à jour formulaire de contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,27 +13,33 @@ const Contact = () => {
                 vous répondrai dans les plus brefs délais.
             </p>
             <br />
-            <form action="">
+            <form
+                action="https://formsubmit.co/cotele_amateur.1e@icloud.com"
+                method="post"
+            >
                 <div className="nom">
-                    <label htmlFor="nom">Nom</label>
-                    <input type="text" id="nom" name="nom" />
+                    <label htmlFor="nom">Nom *</label>
+                    <input type="text" id="nom" name="nom" required />
                 </div>
                 <div className="prenom">
                     <label htmlFor="prenom">Prénom</label>
                     <input type="text" name="prenom" id="prenom" />
                 </div>
                 <div className="email">
-                    <label htmlFor="email">E-mail</label>
-                    <input type="email" name="email" id="email" />
+                    <label htmlFor="email">E-mail *</label>
+                    <input type="email" name="email" id="email" required />
                 </div>
                 <div className="message">
-                    <label htmlFor="message">Message</label>
-                    <textarea name="message" id="message"></textarea>
+                    <label htmlFor="message">Message *</label>
+                    <textarea name="message" id="message" required></textarea>
                 </div>
                 <input type="submit" id="form-submit" />
+                <p>
+                    <em>* Champs requis</em>
+                </p>
             </form>
-            <br/>
-            <SocialNetworks/>
+            <br />
+            <SocialNetworks />
         </section>
     );
 };
