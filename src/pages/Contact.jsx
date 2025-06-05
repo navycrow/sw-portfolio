2d07cb1480f8cdec5f9fe812d45f35a1ba2d3ec8
navycrow <hg64off@gmail.com>
2025-06-05 09:27:33 +0200

    maj contact 4

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,44 +3,55 @@ import SocialNetworks from "../components/SocialNetworks";
 import "./Contact.css";
 
 const Contact = () => {
-    const [nom, setNom] = useState("");
-    const [prenom, setPrenom] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+    // const [nom, setNom] = useState("");
+    // const [prenom, setPrenom] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [message, setMessage] = useState("");
 
-    const [submitted, setSubmitted] = useState(false);
+    // const [submitted, setSubmitted] = useState(false);
     const [msg, setMsg] = useState("");
 
     // Après un envoi réussi, réinitialise le formulaire
-    useEffect(() => {
-        if (submitted)
-            setTimeout(() => {
-                setNom("");
-                setPrenom("");
-                setEmail("");
-                setMessage("");
-                setMsg("");
-                setSubmitted(false);
-            }, 5000);
-    }, [submitted]);
+    // useEffect(() => {
+    //     if (submitted)
+    //         setTimeout(() => {
+    //             setNom("");
+    //             setPrenom("");
+    //             setEmail("");
+    //             setMessage("");
+    //             setMsg("");
+    //             setSubmitted(false);
+    //         }, 5000);
+    // }, [submitted]);
+
+    // function reset(form) {
+    //     setTimeout(() => {
+    //         setMsg("");
+    //         form.reset();
+    //     }, 5000);
+    // }
 
     // Soumission du formulaire
-    function submit(e) {
-        setMsg("Envoi du formulaire en cours...");
+    async function submit(e) {
+        // Annule l'action par défaut et récupère les données du formulaire
         e.preventDefault();
         const form = e.target;
+        const url = form.action;
+        const method = form.method.toUpperCase();
         const formData = new FormData(form);
-        console.log(formData);
+        console.log(url, method, formData);
 
-        fetch("https://formsubmit.co/cotele_amateur.1e@icloud.com", {
-            method: "POST",
+        // Soumet le formulaire via une requête
+        await fetch(url, {
+            method: method,
             body: formData,
         })
             .then((response) => {
                 if (response.ok) {
                     console.log(response);
-                    setSubmitted(true);
+                    // setSubmitted(true);
                     setMsg("Formulaire envoyé avec succès");
+                    // form.reset();
                 } else {
                     setMsg(
                         `Erreur lors de l'envoi du formulaire : ${response.statusText}`
@@ -52,6 +63,12 @@ const Contact = () => {
                     `Erreur lors de l'envoi du formulaire : ${error.message}`
                 )
             );
+
+        // Reset le message et le formulaire au bout de quelques secondes
+        setTimeout(() => {
+            setMsg("");
+            form.reset();
+        }, 5000);
     }
 
     return (
@@ -64,50 +81,58 @@ const Contact = () => {
                 vous répondrai dans les plus brefs délais.
             </p>
             <br />
-            <form onSubmit={submit}>
+            <form
+                action="https://formsubmit.co/cotele_amateur.1e@icloud.com"
+                method="post"
+                onSubmit={submit} // Permet de traiter l'envoi par une fonction et éviter le changement de page
+            >
                 <div className="nom">
                     <label htmlFor="nom">Nom *</label>
                     <input
                         type="text"
                         id="nom"
                         name="nom"
-                        value={nom}
-                        onChange={(e) => setNom(e.target.value)}
+                        // value={nom}
+                        // onChange={(e) => setNom(e.target.value)}
                         required
                     />
                 </div>
+
                 <div className="prenom">
                     <label htmlFor="prenom">Prénom</label>
                     <input
                         type="text"
                         name="prenom"
                         id="prenom"
-                        value={prenom}
-                        onChange={(e) => setPrenom(e.target.value)}
+                        // value={prenom}
+                        // onChange={(e) => setPrenom(e.target.value)}
                     />
                 </div>
+
                 <div className="email">
                     <label htmlFor="email">E-mail *</label>
                     <input
                         type="email"
                         name="email"
                         id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        // value={email}
+                        // onChange={(e) => setEmail(e.target.value)}
                         required
                     />
                 </div>
+                
                 <div className="message">
                     <label htmlFor="message">Message *</label>
                     <textarea
                         name="message"
                         id="message"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
+                        // value={message}
+                        // onChange={(e) => setMessage(e.target.value)}
                         required
                     ></textarea>
                 </div>
 
+                {/* *** Option de formSubmit.co *** */}
                 {/* Objet du mail */}
                 <input
                     type="hidden"
